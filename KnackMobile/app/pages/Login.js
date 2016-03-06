db13ddab1d5512cd6f1c37673ae20f399ac2dfcb
Junoh <junohpark@live.ca>
2016-03-05 16:40:25 -0800

    Done login

--- KnackMobile/app/pages/Login.js
+++ KnackMobile/app/pages/Login.js
@@ -6,6 +6,7 @@ import React, {
 	TextInput,
 	JSONObject,
 	TouchableOpacity,
+	Dimensions,
 } from 'react-native'
 
 // export default allows class to be referenced using import <className> from '<path>'
@@ -13,7 +14,7 @@ export default class Login extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			username: "",
+			email: "",
 			password: "abcd",
 		}
 	}
@@ -26,26 +27,41 @@ export default class Login extends Component {
 	render() {
 		return(
 			<View style={styles.container}>
-				<TextInput style={styles.logininput} 
-					ref= "username"
-					placeholder="Enter email"
-  					placeholderTextColor="gray"
-					onChangeText={(username) => this.setState({username})}
-    				value={this.state.username}/>
+				<View>
+					<TextInput style={styles.logininput} 
+						ref= "email"
+						placeholder="Enter email"
+	  					placeholderTextColor="gray"
+						onChangeText={(email) => this.setState({email})}
+	    				value={this.state.email}/>
+    			</View>
 
-				<TextInput secureTextEntry={true}
-					ref= "password"
-					placeholder="Enter password"
-  					placeholderTextColor="gray"
-					style={styles.logininput}
-					onChangeText={(password)=>this.setState({password})}
-					value={this.state.password}/>
+    			<View>
+					<TextInput secureTextEntry={true}
+						ref= "password"
+						placeholder="Enter password"
+	  					placeholderTextColor="gray"
+						style={styles.logininput}
+						onChangeText={(password)=>this.setState({password})}
+						value={this.state.password}/>
+				</View>
 
-				<TouchableOpacity onPress={this._pushPage.bind(this, 'PageFour', 'PageFour Title')}><Text>Log In</Text></TouchableOpacity>
+				<View style={styles.buttonContainer}>
+					<TouchableOpacity style={styles.button}
+						onPress={this._pushPage.bind(this, 'PageFour', 'PageFour Title')}>
+						<Text>Register</Text>
+					</TouchableOpacity>
+					<TouchableOpacity 
+						onPress={this._pushPage.bind(this, 'PageFour', 'PageFour Title')}>
+						<Text>Log In</Text>
+					</TouchableOpacity>
+				</View>
 			</View>
 		)
 	}
 }
+
+var width = Dimensions.get('window').width; 
 
 var styles = StyleSheet.create({
   container: {
@@ -54,14 +70,23 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'gray',
   },
+
   logininput: {
-    fontSize: 20,
+    fontSize: 14,
     textAlign: 'center',
     backgroundColor: 'white',
     margin: 10,
-    width: 150,
+    width: width * 0.8,
     height: 40
   },
+
+  buttonContainer: {
+  	flexDirection:'row'
+  },
+  
+  button: {
+  	paddingRight: 40
+  }
 });
 
 // This line allows class to be referenced using const <className> = require('<path>')
